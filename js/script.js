fe3b69dd7146dc63aaf2e3cd88786784119c1a6c
Daniele <daniele.bedeschi94@gmail.com>
2020-12-10 11:56:51 +0100

    Print list

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 // - creare una lista di cose da fare e mostrarla in pagina con Vue;
 // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
 // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+
+var app = new Vue (
+  {
+    el: "#root",
+    data: {
+      list: [
+        "Comprare il pane",
+        "Comprare il latte",
+        "Comprare le uova"
+      ]
+    }
+  }
+);
