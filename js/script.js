8b1157991da093190b31382c6f9c5e5c75593f79
Daniele <daniele.bedeschi94@gmail.com>
2020-12-10 11:42:15 +0100

    Commit iniziale

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Esercizio:
+// - creare una lista di cose da fare e mostrarla in pagina con Vue;
+// - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
+// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
