f0169e3ebe1ac63ee6aac6fe3c7bf20ffb538ef0
Daniele <daniele.bedeschi94@gmail.com>
2020-12-10 15:25:17 +0100

    Commit with css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // - creare una lista di cose da fare e mostrarla in pagina con Vue;
 // - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
 // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+// Bonus:
+// - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
 
 var app = new Vue (
   {
